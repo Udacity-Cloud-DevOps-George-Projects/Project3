8175c79e4d18ee1348e5c99de1f35deecff1e50b
George-Gabra <george-gabra@msn.com>
2020-06-28 17:36:39 +0200

    feat: Added Check WebSite Status
    Adding check Website status script
    
    Ref: Udacity Project3

---
+++ CheckWebSiteStatus.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+url='https://udaproject3.s3.amazonaws.com/index.html'
+code=`curl -sL --connect-timeout 20 --max-time 30 -w "%{http_code}\\n" "$url" -o /dev/null`
+
+if [ "$code" = "200" ]; then
+  echo "Website $url is online."
+else
+  echo "Website $url seems to be offline."
+fi
